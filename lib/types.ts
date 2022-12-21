9455f3bb3e8fbee8a368274d5f691cf3a3fa6b86
regirock365 <hemedi@protonmail.com>
2022-12-21 17:23:27 +1030

    add `/api/front-gpt` route

---
+++ lib/types.ts
@@ -0,0 +1,184 @@
+export type GPT3Data = {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  error?: string;
+  usage: {
+    completion_tokens: number;
+    prompt_tokens: number;
+    total_tokens: number;
+  };
+  choices: {
+    text: string;
+    index: number;
+    logprobs: any;
+    finish_reason: string;
+  }[];
+};
+
+export type FrontSearchMessagesReturn = {
+  _pagination: {
+    next: string;
+  };
+  _links: {
+    self: string;
+  };
+  _results: [
+    {
+      _links: {
+        self: string;
+        related: {
+          conversation: string;
+          message_replied_to: string;
+          message_seen: string;
+        };
+      };
+      id: string;
+      type: "email" | "custom"; // "custom"
+      is_inbound: boolean;
+      draft_mode:
+        | "shared"
+        | "private"
+        | "draft"
+        | "sent"
+        | "unsent"
+        | "failed"
+        | null;
+      error_type: string;
+      version: string;
+      created_at: number;
+      subject: string;
+      blurb: string;
+      author?: {
+        _links: {
+          self: string;
+          related: {
+            inboxes: string;
+            conversations: string;
+          };
+        };
+        id: string;
+        email: string;
+        username: string;
+        first_name: string;
+        last_name: string;
+        is_admin: boolean;
+        is_available: boolean;
+        is_blocked: boolean;
+      };
+      recipients: [
+        {
+          _links: {
+            related: {
+              contact: string;
+            };
+          };
+          name: string;
+          handle: string;
+          role: "from" | "to" | "cc" | "bcc";
+        }
+      ];
+      body: string;
+      text: string;
+      attachments: [
+        {
+          id: string;
+          filename: string;
+          url: string;
+          content_type: string;
+          size: number;
+          metadata: {
+            is_inline: boolean;
+            cid: string;
+          };
+        }
+      ];
+      signature: {
+        _links: {
+          self: string;
+          related: {
+            owner: string;
+          };
+        };
+        id: string; // "sig_123"
+        name: string;
+        body: string;
+        sender_info: string;
+        is_visible_for_all_teammate_channels: boolean;
+        is_default: boolean;
+        channel_ids: [string];
+      };
+      metadata: {
+        intercom_url: string;
+        duration: number;
+        have_been_answered: boolean;
+        external_id: string;
+        twitter_url: string;
+        is_retweet: boolean;
+        have_been_retweeted: boolean;
+        have_been_favorited: boolean;
+        thread_ref: string;
+        headers: {};
+        chat_visitor_url: string;
+      };
+    }
+  ];
+};
+
+export type FrontComment = {
+  _links: {
+    self: string;
+  };
+  id: string;
+  type: "comment";
+  emitted_at: number;
+  conversation: {
+    _links: {
+      self: string;
+      related: object;
+    };
+    id: string;
+    subject: string;
+    status: string;
+    assignee: any;
+    recipient: {
+      _links: object;
+      name: string;
+      handle: string;
+      role: string;
+    };
+    tags: object[];
+    links: any[];
+    created_at: number;
+    is_private: boolean;
+    scheduled_reminders: any[];
+    metadata: object;
+  };
+  source: {
+    _meta: { type: string };
+    data: {
+      _links: object;
+      id: string;
+      email: string;
+      username: string;
+      first_name: string;
+      last_name: string;
+      is_admin: boolean;
+      is_available: boolean;
+      is_blocked: boolean;
+      custom_fields: object;
+    };
+  };
+  target: {
+    _meta: { type: string };
+    data: {
+      _links: object;
+      id: string;
+      body: string;
+      posted_at: number;
+      author: object;
+      attachments: any[];
+    };
+  };
+};
---
+++ lib/util.ts
@@ -0,0 +1,39 @@
+export function sortBy<T>(arr: T[], k: keyof T) {
+  return arr.concat().sort((a, b) => (a[k] > b[k] ? 1 : a[k] < b[k] ? -1 : 0));
+}
+
+export function sortByFn<T>(arr: T[], fn: (item: T) => number) {
+  return arr
+    .concat()
+    .sort((a, b) => (fn(a) > fn(b) ? 1 : fn(a) < fn(b) ? -1 : 0));
+}
+
+// a function to extract the main text from an email string
+// leave out the signature, previous emails, etc.
+export function extractMainEmailText(email: string) {
+  // remove text after the signature
+  const signatureLessText = (email + "\n--\n").match(
+    /^[\s\S]*?(?=^[-—_]+$)/m
+  )?.[0];
+
+  // // remove text after the signature
+  // const signatureLessText = collapsedText?.match(/^[\s\S]*?(?=^-- $)/m)?.[0];
+
+  // remove text after the previous emails
+  const previousEmailsLessText = (signatureLessText + "\nOn wrote:")?.match(
+    /^[\s\S]*?(?=^On.*wrote:)/m
+  )?.[0];
+
+  // remove text after previous froms
+  const previousFromsLessText = (previousEmailsLessText + "\nFrom: >")?.match(
+    /^[\s\S]*?(?=^From:.*>)/m
+  )?.[0];
+
+  // collapse multiple newlines
+  const collapsedText = previousFromsLessText; //?.replaceAll(/( *\n *)*/g, "\n");
+
+  // max length of 1000 characters
+  const maxLengthText = collapsedText?.slice(0, 1000);
+
+  return maxLengthText?.trim() ?? "";
+}
