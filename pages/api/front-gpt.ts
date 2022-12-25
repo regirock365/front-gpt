629ca78df0139ca2b18d2227da82d769a15f1c90
regirock365 <hemedi@protonmail.com>
2022-12-25 21:00:17 +1030

    gpt-hello

--- pages/api/front-gpt.ts
+++ pages/api/front-gpt.ts
@@ -39,14 +39,25 @@ export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     console.log("conversationId", conversationId);
 
-    // get the latest comment, for instructions
-    let instructions: string | undefined;
-    {
-      // get all comments in the conversation
-      let conversation_comments: FrontSearchCommentsReturn = await fetch(
+    // check what the command was
+    const comment: string = body.target.data.body;
+    const command = comment.split(" ")[0];
+
+    console.log("command", command);
+
+    if (command === "gpt-response") {
+      // get the instructions, if any
+      let instructions: string | undefined = comment
+        .replace("gpt-response", "")
+        .trim();
+
+      console.log("instructions", instructions);
+
+      // get all messages in the conversation
+      let conversation_messages: FrontSearchMessagesReturn = await fetch(
         `https://api2.frontapp.com/conversations/${encodeURIComponent(
           conversationId
-        )}/comments`,
+        )}/messages`,
         {
           method: "GET",
           headers: {
@@ -59,142 +70,125 @@ export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         .then((response) => response.json())
         .catch((err) => console.error(err));
 
-      const comments = sortByFn(
-        conversation_comments._results,
-        (result) => result.posted_at
-      );
+      const messages = sortByFn(
+        conversation_messages._results,
+        (result) => result.created_at
+      )
+        .filter((result) => result.type === "email")
+        .filter((result) => result.draft_mode !== "private")
+        .map((result) => {
+          // just select the main text, filtering out the signature, etc.
+          const text = extractMainEmailText(result.text);
 
-      // get the latest "gpt-response" comment
-      const latestGPTResponseComment = comments
-        // .filter((result) => result.type === "comment")
-        .filter((result) => result.body.includes("gpt-response"))
-        .pop();
+          return {
+            id: result.id,
+            text,
+            name:
+              result.recipients.find((recipient) => recipient.role === "from")
+                ?.name ||
+              result.recipients.find((recipient) => recipient.role === "from")
+                ?.handle ||
+              "Unknown",
+          };
+        });
 
-      // get the instructions from the latest "gpt-response" comment
-      if (latestGPTResponseComment) {
-        instructions = latestGPTResponseComment.body
-          .replace("gpt-response", "")
-          .trim();
-      }
+      // create the prompt
+      const prompt = `Assistant is a large language model trained by OpenAI.
+  
+  Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+  
+  Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+  
+  Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+  
+  <-user->
+  help me, Hemedi, write a response email, given the following thread. Note, [name] signifies a new email in the thread and {note} signifies instructions or considerations for the response email.
+  ${messages
+    .map(
+      (message) => `[${message.name}]
+  ${message.text}
+  
+  `
+    )
+    .join("")}
+  ${instructions ? `{${instructions}}` : ""}
+  
+  <-assistant->`;
+
+      //   send the messages to GPT-3
+      let gpt_data: GPT3Data = await fetch(
+        `https://api.openai.com/v1/completions`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          },
+          body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt,
+            max_tokens: 256,
+            temperature: 0.7,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            // stop: ["\n"],
+          }),
+        }
+      )
+        .then((response) => response.json())
+        .catch((err) => console.error(err));
+
+      //   parse the response from GPT-3
+      const gpt_response =
+        gpt_data.error ??
+        gpt_data.choices[0].text
+          .split("\n")
+          .filter((line) => line.length > 0)
+          .map((line) => line.trim())
+          .join("\n");
+
+      // add comment to conversation with the message ids
+      let data = await fetch(
+        `https://api2.frontapp.com/conversations/${encodeURIComponent(
+          conversationId
+        )}/comments`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${process.env.FRONT_API_TOKEN}`,
+          },
+          body: JSON.stringify({
+            body: `FrontGPT Responded:\n\n${gpt_response}`,
+          }),
+        }
+      )
+        .then((response) => response.json())
+        .catch((err) => console.error(err));
+    } else if (command === "gpt-hello") {
+      // add comment to conversation with the message ids
+      let data = await fetch(
+        `https://api2.frontapp.com/conversations/${encodeURIComponent(
+          conversationId
+        )}/comments`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${process.env.FRONT_API_TOKEN}`,
+          },
+          body: JSON.stringify({
+            body: `Hello! I'm FrontGPT, a bot that uses GPT-3 to help you out in Front. To get started, just type \`gpt-response\` in a comment and I'll get to work!`,
+          }),
+        }
+      )
+        .then((response) => response.json())
+        .catch((err) => console.error(err));
     }
-
-    console.log("instructions", instructions);
-
-    // get all messages in the conversation
-    let conversation_messages: FrontSearchMessagesReturn = await fetch(
-      `https://api2.frontapp.com/conversations/${encodeURIComponent(
-        conversationId
-      )}/messages`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${process.env.FRONT_API_TOKEN}`,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .catch((err) => console.error(err));
-
-    const messages = sortByFn(
-      conversation_messages._results,
-      (result) => result.created_at
-    )
-      .filter((result) => result.type === "email")
-      .filter((result) => result.draft_mode !== "private")
-      .map((result) => {
-        // just select the main text, filtering out the signature, etc.
-        const text = extractMainEmailText(result.text);
-
-        return {
-          id: result.id,
-          text,
-          name:
-            result.recipients.find((recipient) => recipient.role === "from")
-              ?.name ||
-            result.recipients.find((recipient) => recipient.role === "from")
-              ?.handle ||
-            "Unknown",
-        };
-      });
-
-    // create the prompt
-    const prompt = `Assistant is a large language model trained by OpenAI.
-
-Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
-<-user->
-help me, Hemedi, write a response email, given the following thread. Note, [name] signifies a new email in the thread and {note} signifies instructions or considerations for the response email.
-${messages
-  .map(
-    (message) => `[${message.name}]
-${message.text}
-
-`
-  )
-  .join("")}
-${instructions ? `{${instructions}}` : ""}
-
-<-assistant->`;
-
-    //   send the messages to GPT-3
-    let gpt_data: GPT3Data = await fetch(
-      `https://api.openai.com/v1/completions`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-        body: JSON.stringify({
-          model: "text-davinci-003",
-          prompt,
-          max_tokens: 256,
-          temperature: 0.7,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-          // stop: ["\n"],
-        }),
-      }
-    )
-      .then((response) => response.json())
-      .catch((err) => console.error(err));
-
-    //   parse the response from GPT-3
-    const gpt_response =
-      gpt_data.error ??
-      gpt_data.choices[0].text
-        .split("\n")
-        .filter((line) => line.length > 0)
-        .map((line) => line.trim())
-        .join("\n");
-
-    // add comment to conversation with the message ids
-    let data = await fetch(
-      `https://api2.frontapp.com/conversations/${encodeURIComponent(
-        conversationId
-      )}/comments`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${process.env.FRONT_API_TOKEN}`,
-        },
-        body: JSON.stringify({
-          body: `FrontGPT Responded:\n\n${gpt_response}`,
-        }),
-      }
-    )
-      .then((response) => response.json())
-      .catch((err) => console.error(err));
 
     res.status(200).json({ success: true });
   } catch (error) {
